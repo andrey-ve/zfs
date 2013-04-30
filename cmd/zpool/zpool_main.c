a2d4c76be178a45f81f305c54f30c8d542a425fb
Cyril Plisko <cyril.plisko@infinidat.com>
2013-04-30 15:02:11 +0300

    Disable 'zpool' and 'zfs' commands.
    When operating under IZBOX software standard commands zpool and zfs
    are disabled. Consumers are redirected to izpool and izfs equivalents.

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -5476,6 +5476,16 @@ find_command_idx(char *command, int *idx)
 	return (1);
 }
 
+static void
+izbox_bailout(void)
+{
+	if (getenv("IZBOX_IDDQD") == NULL) {
+		(void) fprintf(stderr, gettext("This command has been "
+		    "disabled. Use izpool command instead.\n"));
+		exit(2);
+	}
+}
+
 int
 main(int argc, char **argv)
 {
@@ -5485,6 +5495,8 @@ main(int argc, char **argv)
 
 	(void) setlocale(LC_ALL, "");
 	(void) textdomain(TEXT_DOMAIN);
+
+	izbox_bailout();
 
 	opterr = 0;
 
