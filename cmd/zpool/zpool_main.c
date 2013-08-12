581dbd6d84ecb5382b1890bec15360e0f57a9a5c
Cyril Plisko <cyril.plisko@mountall.com>
2013-08-12 12:31:50 +0300

    Revert "Disable 'zpool' and 'zfs' commands."
    This reverts commit e60cdaaa653c0cad96d7ad8347bccd0d15be780d.

--- cmd/zpool/zpool_main.c
+++ cmd/zpool/zpool_main.c
@@ -5669,16 +5669,6 @@ find_command_idx(char *command, int *idx)
 	return (1);
 }
 
-static void
-izbox_bailout(void)
-{
-	if (getenv("IZBOX_IDDQD") == NULL) {
-		(void) fprintf(stderr, gettext("This command has been "
-		    "disabled. Use izpool command instead.\n"));
-		exit(2);
-	}
-}
-
 int
 main(int argc, char **argv)
 {
@@ -5688,8 +5678,6 @@ main(int argc, char **argv)
 
 	(void) setlocale(LC_ALL, "");
 	(void) textdomain(TEXT_DOMAIN);
-
-	izbox_bailout();
 
 	opterr = 0;
 
