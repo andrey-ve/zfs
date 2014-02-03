4ff928dcb239e7002bcf4f9301676dc0528c60b9
Cyril Plisko <cyril.plisko@mountall.com>
2014-02-03 11:12:47 +0200

    Import time optimization and instrumentation
    o IZBOX_DEV_FILTER - Ignore devices without -part1 suffix
    o IZBOX_TIMING - Collect timing of major import phases

--- lib/libzfs/libzfs_import.c
+++ lib/libzfs/libzfs_import.c
@@ -1036,6 +1036,10 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 	vdev_entry_t *ve, *venext;
 	config_entry_t *ce, *cenext;
 	name_entry_t *ne, *nenext;
+	boolean_t part1only = B_FALSE;
+	boolean_t timing = B_FALSE;
+	hrtime_t start = 0;
+	hrtime_t elapsed_ms = 0;
 
 	verify(iarg->poolname == NULL || iarg->guid == 0);
 
@@ -1099,6 +1103,14 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			goto error;
 		}
 
+		if (getenv("IZBOX_DEV_FILTER") != NULL)
+		    part1only = B_TRUE;
+
+		if (getenv("IZBOX_TIMING") != NULL)
+			timing = B_TRUE;
+
+		if (timing)
+			start = gethrtime();
 		/*
 		 * This is not MT-safe, but we have no MT consumers of libzfs
 		 */
@@ -1107,6 +1119,20 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			if (name[0] == '.' &&
 			    (name[1] == 0 || (name[1] == '.' && name[2] == 0)))
 				continue;
+
+			/*
+			 * In IZBox environment we want to ignore everything
+			 * that doesn't have -part1 suffix
+			 */
+			if (part1only) {
+				const char *suffix = strrchr(name, '-');
+
+				if ((suffix == NULL) ||
+				    (strncmp(suffix, "-part1", 6) != 0)) {
+					continue;
+				}
+			}
+
 
 			/*
 			 * Skip checking devices with well known prefixes:
@@ -1187,6 +1213,12 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			}
 		}
 
+		if (timing) {
+			elapsed_ms = (gethrtime() - start) / 1000000;
+			printf("Scan took %llu millisec\n", elapsed_ms);
+		}
+
+
 		(void) closedir(dirp);
 		dirp = NULL;
 	}
@@ -1194,7 +1226,16 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 #ifdef HAVE_LIBBLKID
 skip_scanning:
 #endif
+
+	if (timing)
+		start = gethrtime();
+
 	ret = get_configs(hdl, &pools, iarg->can_be_active);
+
+	if (timing) {
+		elapsed_ms = (gethrtime() - start) / 1000000;
+		printf("get_configs() took %llu millisec\n", elapsed_ms);
+	}
 
 error:
 	for (pe = pools.pools; pe != NULL; pe = penext) {
